b8b4240f03d5c5ded4ea9c6e4eab1e9cc6af9e35
Edward Mahama <edwardmahama@microsoft.com>
2023-05-24 13:04:55 -0400

    add app's entry

---
+++ Building-Apps-React-Redux/src/index.js
@@ -0,0 +1,9 @@
+// This is the entry point for our app
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <p>Hi.</p>;
+};
+
+render(<App />, document.getElementById("app"));
