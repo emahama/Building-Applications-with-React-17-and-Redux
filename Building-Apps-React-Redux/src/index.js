ad5b3351127f065219867b130f9be213a03e2085
Edward Mahama <edwardmahama@microsoft.com>
2023-05-24 14:09:13 -0400

    react components types

--- Building-Apps-React-Redux/src/index.js
+++ Building-Apps-React-Redux/src/index.js
@@ -2,8 +2,23 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = () => {
-  return <p>Hi.</p>;
+const App = (props) => {
+  return <p>Hi. from {props.message}</p>;
 };
 
-render(<App />, document.getElementById("app"));
+function Hello(props) {
+  return <div>Hello world {props.message}</div>;
+}
+
+class Hello1 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div>Hello world from {this.props.message}</div>;
+  }
+}
+
+// render(<Hello1 message="class component" />, document.getElementById("app"));
+// render(<Hello message="function component" />, document.getElementById("app"));
+render(<App message="Arrow function " />, document.getElementById("app"));
