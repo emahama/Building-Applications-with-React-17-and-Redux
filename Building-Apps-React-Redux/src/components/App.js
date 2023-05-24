b2933d76587b830941e81ffcd34ba229f69c04a4
Edward Mahama <edwardmahama@microsoft.com>
2023-05-24 16:03:32 -0400

    add pageNotFound and course component

--- Building-Apps-React-Redux/src/components/App.js
+++ Building-Apps-React-Redux/src/components/App.js
@@ -2,13 +2,19 @@ import React from "react";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
 import { Route, Switch } from "react-router-dom";
+import Header from "./common/Header";
+import PageNotFound from "./common/PageNotFound";
+import CoursesPage from "./courses/CoursesPage";
 
 const App = () => {
   return (
     <div className="container-fluid">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/courses" component={CoursesPage} />
+        <Route component={PageNotFound} />
       </Switch>
     </div>
   );
---
+++ Building-Apps-React-Redux/src/components/common/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageNotFound = () => (
+  <div>
+    <p>Opps! Page not found. </p>
+  </div>
+);
+
+export default PageNotFound;
