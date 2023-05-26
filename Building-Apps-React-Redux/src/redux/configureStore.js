275b4580f1b327d10154f4fd356d33a160b589f9
Edward Mahama <edwardmahama@microsoft.com>
2023-05-26 16:11:20 -0400

    enhance middleware to for us to use redux devtools

--- Building-Apps-React-Redux/src/redux/configureStore.js
+++ Building-Apps-React-Redux/src/redux/configureStore.js
@@ -1,11 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducer";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 
 export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(reduxImmutableStateInvariant())
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
   );
 }
+
+/**
+ * NB: applyMiddleware(reduxImmutableStateInvariant()) this middleware will warn us when we mutate state in the store
+ * Wrapping composeEnhancers will enable us to use redux devtool in the browser
+ *
+ */
