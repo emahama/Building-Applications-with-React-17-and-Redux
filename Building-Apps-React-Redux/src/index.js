1c27e5601281c6aa0aa7623c6c228a9476740351
Edward Mahama <edwardmahama@microsoft.com>
2023-05-26 16:35:10 -0400

    Configure the store at the app entry point

--- Building-Apps-React-Redux/src/index.js
+++ Building-Apps-React-Redux/src/index.js
@@ -5,12 +5,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
-/*
-The purpose of <Router> is to wrap the application components and enable the implementation of client-side routing.
-* */
+import configureStore from "./redux/configureStore";
+import { Provider as ReduxProvider } from "react-redux";
+
+const store = configureStore();
+
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
+
+/*
+The purpose of <Router> is to wrap the application components and enable the implementation of client-side routing.
+*  <ReduxProvider store={store}> configure our app and its children to access the store
+*/
