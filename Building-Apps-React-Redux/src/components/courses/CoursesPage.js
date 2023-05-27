feb9ad3c8a043efa62f45d751f35f73d8a8ed69a
Edward Mahama <edwardmahama@microsoft.com>
2023-05-27 00:01:33 -0400

    convert class to function

--- Building-Apps-React-Redux/src/components/courses/CoursesPage.js
+++ Building-Apps-React-Redux/src/components/courses/CoursesPage.js
@@ -1,47 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
-class CoursesPage extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-  state = {
-    course: {
-      title: "",
-    },
+const CoursesPage = (props) => {
+  const [course, setCourse] = useState({ title: "" });
+
+  const handleChange = (event) => {
+    const newCourse = { ...course, title: event.target.value };
+    setCourse(newCourse);
   };
 
-  handleChange = (event) => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course: course });
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
+    props.actions.createCourse(course);
     // alert(this.state.course.title);
   };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
-        {this.props.courses.map((course, index) => (
-          <div key={index}>{course.title}</div>
-        ))}
-      </form>
-    );
-  }
-}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type="text" onChange={handleChange} value={course.title} />
+      <input type="submit" value="Save" />
+      {props.courses.map((course, index) => (
+        <div key={index}>{course.title}</div>
+      ))}
+    </form>
+  );
+};
 
 CoursesPage.propTypes = {
   actions: PropTypes.object.isRequired,
